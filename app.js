7530b94998a2f861aaf74af31e6247a8f96c9e23
Tia Fields <tia.fields226@gmail.com>
2023-08-11 20:41:40 -0400

    adding partials to views folder, and they are functioning

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const express = require('express');
 const app = express();
 
 //register view engine 
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) =>{
     res.render('index');
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about');
+})
+
+app.use((req, res) => {
+    res.status(404).render('404');
 })
